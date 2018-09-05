26442dd4451bcc606d78048a4f0e9f4a984f39ee
Zak Laughton <zlaughton@gmail.com>
2018-09-04 21:55:16 -0500

    feat: add offline capability.

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    new Response('Hello world')
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('restaurant-reviews').then(cache => {
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js'
+      ])
+    })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return (response ? response : fetch(event.request));
+    })
+  )
+})
